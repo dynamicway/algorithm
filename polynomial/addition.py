e0492b2838814bd9b1d6eb410c145e8fd3b89f4e
bee <rkddbtjd123@gmail.com>
2025-07-29 08:58:50 +0900

    refactor: consolidate polynomial functions

--- polynomial/addition.py
+++
@@ -1,4 +0,0 @@
-from itertools import zip_longest
-
-def add(a, b):
-    return [x + y for x, y in zip_longest(a, b, fillvalue=0)]
--- polynomial/polynomial.py
+++ polynomial/polynomial.py
@@ -1,3 +1,7 @@
+
+from itertools import zip_longest
+
+
 def multiply(a, b):
     result = [0] * (len(a) + len(b) - 1)
     for i in range(len(a)):
@@ -5,3 +9,6 @@ def multiply(a, b):
             result[i + j] += a[i] * b[j]
     
     return result
+
+def add(a, b):
+    return [x + y for x, y in zip_longest(a, b, fillvalue=0)]
