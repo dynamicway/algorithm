f28a9a27385daf677629e0a4cd004020680b1d8f
bee <rkddbtjd123@gmail.com>
2025-08-16 02:11:00 +0900

    feat: Improve performance

--- src/queue/baekjoon/baekjoon_1158.py
+++ src/queue/baekjoon/baekjoon_1158.py
@@ -1,15 +1,18 @@
+from collections import deque
+
+
 def josephus(n, k):
-    q1 = list(range(1, n + 1))
-    q2 = []
+    q1 = deque(range(1, n + 1))
+    q2 = deque()
     
     result = []
     while len(result) != n:
         cn = len(q1) + len(q2)
         if cn == 1:
             if q1:
-                result.append(q1.pop())
+                result.append(q1.popleft())
             else:
-                result.append(q2.pop())
+                result.append(q2.popleft())
             break
         
         remove_point = k
@@ -20,11 +23,11 @@ def josephus(n, k):
         for i in range(k):
             if len(q1) == 0:
                 q1 = q2
-                q2 = []
+                q2 = deque()
             if i == k - 1:
-                result.append(q1.pop(0))
+                result.append(q1.popleft())
             else:
-                q2.append(q1.pop(0))
+                q2.append(q1.popleft())
 
     return result
 
