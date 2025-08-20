0e2f0f8ffe41617f2d6452fea0d4b4dc03ffee6e
bee <rkddbtjd123@gmail.com>
2025-08-20 09:31:27 +0900

    refactor: Simplify count_pieces function logic in baekjoon 10799

--- src/stack/baekjoon/baekjoon_10799.py
+++ src/stack/baekjoon/baekjoon_10799.py
@@ -1,20 +1,17 @@
-import sys
-
 def count_pieces(layout):
     result = 0
-    stack = []
+    cutting_bars = 0
 
     i = 0
     while i < len(layout):
-        if layout[i] == "(":
-            if layout[i + 1] == "(":
-                result += 1
-                stack.append("(")
-            else:
-                result += len(stack)
-                i += 1
+        if layout[i] == "(" and layout[i + 1] == "(": # 새로운 쇠막대기 추가
+            result += 1
+            cutting_bars += 1
+        elif layout[i] == "(" and layout[i + 1] == ")": ## 쇠막대기들 자름
+            result += cutting_bars
+            i += 1 ## 레이저 끝 부분은 넘어감
         else:
-            stack.pop()
+            cutting_bars -= 1 ## 잘라진 쇠막대기 제거
         i += 1
 
     return result
